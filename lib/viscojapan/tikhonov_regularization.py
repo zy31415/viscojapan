da405393bfc5f4d0457e77e6e8f32e3b7238d018
Yang Zhang <zy31415@gmail.com>
2014-06-03 16:17:59 -0700

    Regularization matrix.

--- lib/viscojapan/tikhonov_regularization.py
+++ lib/viscojapan/tikhonov_regularization.py
@@ -1,11 +1,50 @@
-from scipy.sparse import block_diag, bmat, eye
+from scipy.sparse import block_diag, bmat, eye, block_diag, coo_matrix, hstack
 
-class TikhonovSecondOrder:
+def hstack_zeros_padding(mat, num_cols):
+    ''' Add zeros columns for non linear parameters
+'''
+    sh = mat.shape
+    padding = coo_matrix((sh[0], num_cols),dtype='float')
+    mat = hstack([mat,padding])
+    return mat
+
+class TikhonovRegularization:
+    ''' Base class defines the interface.
+'''
+    def __init__(self):
+        pass
+
+    def regularization_matrix_each_epoch(self):
+        ''' Return regularization matrix
+'''
+        raise NotImplementedError("Implement this function.")
+
+    def regularization_matrix(self):
+        mat = self.regularization_matrix_each_epoch()
+        blocks = [mat]*self.num_epochs
+        mat = block_diag(blocks, dtype = 'float')
+        mat = hstack_zeros_padding(mat, self.num_nlin_pars)
+        return mat
+
+class TikhonovZerothOrder(TikhonovRegularization):
     def __init__(self, nrows_slip, ncols_slip):
+        super().__init__()
+        self.nrows_slip = nrows_slip
+        self.ncols_slip = ncols_slip
+        self.num_epochs = None
+        
+    def regularization_matrix_each_epoch(self):
+        return eye(self.nrows_slip * self.ncols_slip, dtype='float')
+    
+class TikhonovSecondOrder(TikhonovRegularization):
+    def __init__(self, nrows_slip, ncols_slip):
+        super().__init__()
         self.nrows_slip = nrows_slip
         self.ncols_slip = ncols_slip
         self.row_norm_length = None
         self.col_norm_length = None
+        self.num_epochs = None
+        self.num_nlin_pars = None
 
     def roughening_matrix(self, num_cols):
         ''' Return any size of roughening matrix.
@@ -66,14 +105,23 @@ class TikhonovSecondOrder:
         return bmat(block, dtype='float')
 
     def row_col_roughening_normed(self):
-        return self.row_col_roughening_norm() / self.row_norm_length / self.col_norm_length
+        return self.row_col_roughening() / self.row_norm_length / self.col_norm_length
 
-    def regularization_matrix(self):
+    def regularization_matrix_unnormed(self):
         row = self.row_roughening()
         col = self.col_roughening()
         row_col = self.row_col_roughening()
 
         return row.T.dot(row) + col.T.dot(col) +row_col.T.dot(row_col)
+    
+    def regularization_matrix_each_epoch(self):
+        row = self.row_roughening_normed()
+        col = self.col_roughening_normed()
+        row_col = self.row_col_roughening_normed()
+
+        return row.T.dot(row) + col.T.dot(col) +row_col.T.dot(row_col)
+
+
         
     
         
