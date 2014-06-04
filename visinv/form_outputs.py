602cec944e50f93983faa1dbf6663f78e0a763cd
Yang Zhang <zy31415@gmail.com>
2014-06-04 14:59:03 -0700

    .

--- visinv/form_outputs.py
+++ visinv/form_outputs.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+from os.path import join
 
 from numpy import logspace
 
@@ -10,8 +11,9 @@ from viscojapan.post_inversion import InversionResults
 from viscojapan.slip import incr_slip_to_slip
 from days import days
 
+path_outs = './outs_tik2/'
 for ano, alpha in enumerate(logspace(-5,3,30)):
-    with open('outs/res_%02d.pkl'%ano,'rb') as fid:
+    with open(join(path_outs,'res_%02d.pkl'%ano),'rb') as fid:
         alpha, sol = pickle.load(fid)
 
     invres = InversionResults()
@@ -22,7 +24,7 @@ for ano, alpha in enumerate(logspace(-5,3,30)):
     invres.init()
     info_dic = {'alpha':alpha,
                 'tik_order':2,}
-    incr_slip_file = 'outs/incr_slip_%02d.h5'%ano
-    slip_file = 'outs/slip_%02d.h5'%ano
+    incr_slip_file = join(path_outs, 'incr_slip_%02d.h5'%ano)
+    slip_file = join(path_outs, 'slip_%02d.h5'%ano)
     invres.gen_inverted_incr_slip_file(incr_slip_file, info_dic)
     incr_slip_to_slip(incr_slip_file, slip_file)
