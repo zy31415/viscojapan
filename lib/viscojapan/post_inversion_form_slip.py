7159ee11b150d4bf5c9a9cb09031519ee6fd4838
Yang Zhang <zy31415@gmail.com>
2014-06-04 20:18:16 -0700

    Post-processing

---
+++ lib/viscojapan/post_inversion_form_slip.py
@@ -0,0 +1,50 @@
+from numpy import asarray
+
+from .slip import EpochalIncrSlip, EpochalSlip
+
+class FormSlip(object):
+    def __init__(self):
+        self.solution = None
+        self.epochs = None
+        self.nlin_par_names = []
+
+        self.num_of_subfaults = 250
+
+    def init(self):
+        self.num_of_nlin_pars = len(self.nlin_par_names)
+        self.incr_slip_arr = asarray(self.solution['x'])[0:-self.num_of_nlin_pars]
+        self.nlin_par_vals = asarray(self.solution['x'])[-self.num_of_nlin_pars:]
+        self.nlin_par_vals = self.nlin_par_vals.flatten()
+        
+        for pn, val in zip(self.nlin_par_names,
+                           self.nlin_par_vals):
+            setattr(self,pn,val)
+
+    def gen_inverted_incr_slip_file(self, incr_slip_file, info_dic={}):
+        incr_slip = EpochalIncrSlip(incr_slip_file)
+        for nth, epoch in enumerate(self.epochs):
+            incr_slip.set_epoch_value(epoch,self.incr_slip_arr[
+                nth*self.num_of_subfaults:(nth+1)*self.num_of_subfaults, 0])
+
+        incr_slip.set_info('incr_slip_arr', self.incr_slip_arr)
+        incr_slip.set_info('num_of_subfaults', self.num_of_subfaults)
+        
+        for par in self.nlin_par_names:
+            incr_slip.set_info(par, getattr(self,par))
+        incr_slip.set_info_dic(info_dic)
+
+    def gen_inverted_slip_file(self, slip_file):
+        slip = EpochalSlip(slip_file)
+        for nth, epoch in enumerate(self.epochs):
+            if nth == 0:
+                val0 = self.incr_slip_arr[nth*self.num_of_subfaults:
+                                          (nth+1)*self.num_of_subfaults,0]
+            else:
+                val = self.incr_slip_arr[nth*self.num_of_subfaults:
+                                         (nth+1)*self.num_of_subfaults,0]
+                val0 += val
+            slip.set_epoch_value(epoch,val0)
+
+    def get_predicated_obs(self):
+        pass
+        
---
+++ visinv/form_slip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+import pickle
+from os.path import join
+
+from numpy import logspace
+
+sys.path.append('/home/zy/workspace/viscojapan/lib')
+from viscojapan.post_inversion_form_slip import FormSlip
+from viscojapan.slip import incr_slip_to_slip
+from days import days
+
+path_outs = './outs_tik2/'
+for ano, alpha in enumerate(logspace(-5,3,30)):
+    with open(join(path_outs,'res_%02d.pkl'%ano),'rb') as fid:
+        alpha, sol = pickle.load(fid)
+
+    invres = FormSlip()
+    invres.solution = sol
+    invres.epochs = days
+    invres.nlin_par_names = ['log10_visM']
+
+    invres.init()
+    info_dic = {'alpha':alpha,
+                'tik_order':2,}
+    incr_slip_file = join(path_outs, 'incr_slip_%02d.h5'%ano)
+    slip_file = join(path_outs, 'slip_%02d.h5'%ano)
+    invres.gen_inverted_incr_slip_file(incr_slip_file, info_dic)
+    incr_slip_to_slip(incr_slip_file, slip_file)
