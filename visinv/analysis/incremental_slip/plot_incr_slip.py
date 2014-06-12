a1cfd0b527026810bb4cd8bf065c3c641bbba644
Yang Zhang <zy31415@gmail.com>
2014-06-12 14:10:42 -0700

    Adjust the structure.

--- visinv/analysis/incremental_slip/plot_incr_slip.py
+++ visinv/analysis/incremental_slip/plot_incr_slip.py
@@ -8,7 +8,7 @@ from viscojapan.slip import EpochalIncrSlip
 from days import days as epochs
 
 def plot_incr_slip(ano,epoch):
-    slip = EpochalIncrSlip('../outs_tik2/incr_slip_%02d.h5'%ano)
+    slip = EpochalIncrSlip('../../outs_tik2/incr_slip_%02d.h5'%ano)
     epochs = slip.get_epochs()
 
     alpha = slip.get_info('alpha')
@@ -22,23 +22,6 @@ def plot_incr_slip(ano,epoch):
     m.plot_fslip(co_slip)
     
     append_title(', ano = %d, alpha=%.3f, epoch=%d'%(ano,alpha,epoch))
-
-##for ano in range(0,30):    
-##    slip = EpochalSlip('../outs_tik2/slip_%02d.h5'%ano)
-##    epochs = slip.get_epochs()
-##
-##    print(ano, slip.get_info('alpha'))
-##
-##    co_slip = slip.get_epoch_value(0)
-##
-##    m = Map()
-##    m.region_code = 'near'
-##    m.init()
-##    s = slip.get_epoch_value(epochs[0])
-##    m.plot_fslip(co_slip)
-##
-##    show()
-
 
 ano = 15
 
