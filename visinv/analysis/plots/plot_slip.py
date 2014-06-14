e3321e3802e7709038238cfac354c95ddb7be433
Yang Zhang <zy31415@gmail.com>
2014-06-14 15:48:07 -0700

    .

--- visinv/analysis/plots/plot_slip.py
+++ visinv/analysis/plots/plot_slip.py
@@ -8,21 +8,39 @@ from viscojapan.slip import EpochalIncrSlip
 from days import days as epochs
 
 def plot_incr_slip(ano,epoch):
-    slip = EpochalIncrSlip('../../outs_tik2/incr_slip_%02d.h5'%ano)
+    slip = EpochalIncrSlip('../outs_tik2/incr_slip_%02d.h5'%ano)
     epochs = slip.get_epochs()
 
     alpha = slip.get_info('alpha')
 
-    s = slip.get_epoch_value(epoch)
+    co_slip = slip.get_epoch_value(epoch)
 
     m = Map()
     m.region_code = 'near'
     m.init()
-    m.plot_fslip(s)
+    s = slip.get_epoch_value(epochs[0])
+    m.plot_fslip(co_slip)
     
     append_title(', ano = %d, alpha=%.3f, epoch=%d'%(ano,alpha,epoch))
 
-ano = 16
+##for ano in range(0,30):    
+##    slip = EpochalSlip('../outs_tik2/slip_%02d.h5'%ano)
+##    epochs = slip.get_epochs()
+##
+##    print(ano, slip.get_info('alpha'))
+##
+##    co_slip = slip.get_epoch_value(0)
+##
+##    m = Map()
+##    m.region_code = 'near'
+##    m.init()
+##    s = slip.get_epoch_value(epochs[0])
+##    m.plot_fslip(co_slip)
+##
+##    show()
+
+
+ano = 15
 
 for epoch in epochs:
     plot_incr_slip(ano, epoch)
