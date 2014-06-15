3d9994071ff4e49c7d242e2bc851fb08691d3f54
Yang Zhang <zy31415@gmail.com>
2014-06-14 22:21:40 -0700

    Optimize OccamInversion.

--- lib/viscojapan/occam_algorithm/occam_inversion.py
+++ lib/viscojapan/occam_algorithm/occam_inversion.py
@@ -25,14 +25,26 @@ class OccamInversion:
 
         self.epochs = []
 
-    def _formulate_occam(self): 
+    def _formulate_occam(self):
+        G1 = EpochalG(self.file_G1, self.sites_file)
+        G2 = EpochalG(self.file_G2, self.sites_file)
+        dG = DiffED(G1, G2, 'log10_visM')
+
+        obs = EpochalDisplacement(self.f_d, self.sites_file)
+
+        self.visM = G1.get_info('visM')
+        self.log10_visM = log10(self.visM)
+        print('Initial Maxwellian viscosity: %g'%self.visM)
+
+        jac_1 = JacobianVec(dG, self.f_slip0)
+        
         formulate_occam = FormulatOccam()
         formulate_occam.epochs = self.epochs
         formulate_occam.non_lin_par_vals = [self.log10_visM]
-        formulate_occam.non_lin_JacobianVecs = [self.jac_1]
-        formulate_occam.G = self.G1
-        formulate_occam.d = self.obs
-        self.formulate_occam = formulate_occam
+        formulate_occam.non_lin_JacobianVecs = [jac_1]
+        formulate_occam.G = G1
+        formulate_occam.d = obs
+        return formulate_occam
 
     def _tikhonov_regularization(self):
         # regularization
@@ -41,57 +53,24 @@ class OccamInversion:
         reg.col_norm_length = 28./23.03
         reg.num_epochs = len(self.epochs)
         reg.num_nlin_pars = 1
-        self.tikhonov_regularization = reg
+        return reg
         
-    def init(self):
-        self.G1 = EpochalG(self.file_G1, self.sites_file)
-        G2 = EpochalG(self.file_G2, self.sites_file)
-        self.dG = DiffED(self.G1, G2, 'log10_visM')
-
-        self.obs = EpochalDisplacement(self.f_d, self.sites_file)
-
-        self.visM = self.G1.get_info('visM')
-        self.log10_visM = log10(self.visM)
-        print('Initial Maxwellian viscosity: %g'%self.visM)
-
-        self.jac_1 = JacobianVec(self.dG, self.f_slip0)
-
-        self._formulate_occam()
-
-        self.d_ = self.formulate_occam.d_()
-        self.jacobian = self.formulate_occam.Jacobian()
-
-        self._tikhonov_regularization()
-
-        # Inversion
-        self._least_square()
 
     def invert(self, alpha):
         least_square = LeastSquare()
-        least_square.G = self.jacobian
-        least_square.d = self.d_
-        least_square.tikhonov_regularization = self.tikhonov_regularization
+
+        formulate_occam = self._formulate_occam()
+        least_square.G = formulate_occam.jacobian()
+        least_square.d = formulate_occam.d_()
+
+        least_square.tikhonov_regularization = self._tikhonov_regularization()
         
         least_square.alpha = alpha
         self.alpha =  alpha
         
         self.solution = least_square()
-    
-    def save_raw(self, file_name):
-        with open(file_name,'wb') as fid:
-            pickle.dump((self.least_square.alpha, self.solution),fid)
 
-    def load_raw(self, file_name):
-        with open(file_name,'rb') as fid:
-            self.alpha, self.solution = \
-                   pickle.load(fid)
-
-    def pickle(self,fn):       
-
-        self.jacobian = None
-        self.d_ = None
-        self.tikhonov_regularization = None
-        
+    def pickle(self,fn):        
         with open(fn, 'wb') as fid:
             pickle.dump(self, fid)
 
