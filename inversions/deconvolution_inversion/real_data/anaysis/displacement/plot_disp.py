c117596cc01311c715921b558de0743a4091ce06
Yang Zhang <zy31415@gmail.com>
2014-06-25 12:50:49 -0700

    .

--- inversions/deconvolution_inversion/real_data/anaysis/displacement/plot_disp.py
+++ inversions/deconvolution_inversion/real_data/anaysis/displacement/plot_disp.py
@@ -14,16 +14,18 @@ aslip_obs = disp_obs.get_epoch_value(epoch) - disp_obs.get_epoch_value(0)
 sites = disp_obs.get_info('sites')
 ano = 13
 bno = 15
-disp_pred = EpochalDisplacement('../../outs_log/pred_disp_a%02d_b%02d.h5'%(ano,bno),
-                           file_sites)
+##disp_pred = EpochalDisplacement('../../outs_log/pred_disp_a%02d_b%02d.h5'%(ano,bno),
+##                           file_sites)
+
+disp_pred = EpochalDisplacement('../../res_pred_disp.h5', file_sites)
 
 aslip_pred = disp_pred.get_epoch_value(epoch) - disp_pred.get_epoch_value(0)
 
 m = Map()
-m.region_code = 'I'
+m.region_code = 'near'
 m.init()
 
-scale = 0.5
+scale = 10
 m.plot_disp(aslip_obs,sites, scale=scale)
 m.plot_disp(aslip_pred,sites,color='red', scale=scale)
 
