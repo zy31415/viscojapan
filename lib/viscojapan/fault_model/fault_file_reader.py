2f664f12a686ad0c13c21000eebf104054a999d8
Yang Zhang <zy31415@gmail.com>
2014-07-02 14:38:22 -0700

    next step: 1. fault file reader and writer 2. plot trench and slab

--- lib/viscojapan/fault_model/fault_file_reader.py
+++ lib/viscojapan/fault_model/fault_file_reader.py
@@ -1,10 +1,8 @@
-from os.path import exists
 import h5py
 
-class FaultFileReader(object):
+class FaultFileIO(object):
     def __init__(self, fault_file):
         self.fault_file = fault_file
-        assert exists(self.fault_file), "File does not exist."
 
     def get_num_subflts_in_strike(self):
         with h5py.File(self.fault_file, 'r') as fid:
