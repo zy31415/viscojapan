92472f9bddf5b1c23eba617710dae777e02a80d5
Yang Zhang <zy31415@gmail.com>
2014-07-06 00:49:43 -0700

    finish dpool

--- tests/test_dpool/test_task.py
+++ tests/test_dpool/test_task.py
@@ -8,8 +8,4 @@ tasks = []
 for n in range(20):
     tasks.append(Task(target = sleep,
                       args = (randrange(10),)))
-dp = DPool(
-    tasks=tasks,
-    controller_file='share/pool.config.dynamic')
-
-dp.run()
+print(tasks[0])
