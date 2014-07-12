b8ff93a16a7b877582aca2aab277075c044e34d6
Yang Zhang <zy31415@gmail.com>
2014-07-12 00:00:44 -0700

    .

--- inversions/static_inversion/coseismic_inversion_wider/greens_function/compute_greens_func_mp.py
+++ inversions/static_inversion/coseismic_inversion_wider/greens_function/compute_greens_func_mp.py
@@ -3,11 +3,13 @@ import glob
 from viscojapan.pollitz.compute_greens_function \
      import ComputeGreensFunction
 
+from epochs import epochs
+
 subflts_files = \
               sorted(glob.glob('../fault_model/subflts/flt_????'))
 
 com = ComputeGreensFunction(
-    epochs = [0],
+    epochs = epochs,
     file_sites = 'sites_with_seafloor',
     earth_file = '../earth_model_He50km/earth.model_He50km',
     earth_file_dir = '../earth_model_He50km/earth_files/',
