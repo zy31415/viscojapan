c74cfa0f3aac35ede318493d08b56a1ea1ebf87b
Yang Zhang <zy31415@gmail.com>
2014-07-18 01:26:44 -0700

    Add a feeder process.

--- lib/dpool/feeder.py
+++ lib/dpool/feeder.py
@@ -7,7 +7,8 @@ class Feeder(Process):
         self.dp_state = dp_state        
 
     def run(self):
-        for task in tasks:
+        for task in self.tasks:
+            print('    Add %s '%str(task))
             self.dp_state.q_waiting.put(task)
         
         
