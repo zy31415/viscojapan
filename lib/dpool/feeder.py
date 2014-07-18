392680b1d8154154a8ec092bb4362cd24600db44
Yang Zhang <zy31415@gmail.com>
2014-07-18 01:23:24 -0700

    Add a feeder process.

--- lib/dpool/feeder.py
+++ lib/dpool/feeder.py
@@ -2,6 +2,7 @@ from multiprocessing import Process
 
 class Feeder(Process):
     def __init__(self, tasks, dp_state):
+        super().__init__()
         self.tasks = tasks
         self.dp_state = dp_state        
 
