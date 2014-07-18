5a3e5805dc8bc5e6a2d0bba0af3f5b88aff0973d
Yang Zhang <zy31415@gmail.com>
2014-07-17 21:17:08 -0700

    .

--- inversions/inversion3/earth_model/compute_He45km.py
+++ inversions/inversion3/earth_model/compute_He45km.py
@@ -1,4 +1,4 @@
 from compute_earth_model import cmd1_He45km, cmd2_He45km
 
 cmd1_He45km()
-cmd2_He45km()
+cmd2_He45km.run()
--- inversions/inversion3/earth_model/compute_He55km.py
+++ inversions/inversion3/earth_model/compute_He55km.py
@@ -1,4 +1,4 @@
 from compute_earth_model import cmd1_He55km, cmd2_He55km
 
 cmd1_He55km()
-cmd2_He55km()
+cmd2_He55km.run()
