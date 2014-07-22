3d27d89c749dd84b952aa19e15d68905951f09d7
Yang Zhang <zy31415@gmail.com>
2014-07-22 14:46:10 -0700

    .

--- lib/viscojapan/plots/__init__.py
+++ lib/viscojapan/plots/__init__.py
@@ -1,11 +1,12 @@
 try:
     from pylab import plt
+    from .my_basemap import MyBasemap
+    from .map_plot import MapPlot
+    from .map_plot_slab import MapPlotSlab
+    from .map_plot_fault import MapPlotFault
+    from .map_plot_sites import MapPlotDisplacement
+    from .plot_L_curve import plot_L
 except ImportError:
     pass
 
-from .my_basemap import MyBasemap
-from .map_plot import MapPlot
-from .map_plot_slab import MapPlotSlab
-from .map_plot_fault import MapPlotFault
-from .map_plot_sites import MapPlotDisplacement
-from .plot_L_curve import plot_L
+
