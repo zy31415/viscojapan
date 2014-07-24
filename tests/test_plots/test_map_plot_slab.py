a9c93cbf49d2b82383e4d3c4c7e620f17343cc29
Yang Zhang <zy31415@gmail.com>
2014-07-24 10:07:41 -0700

    .

--- tests/test_plots/test_map_plot_slab.py
+++ tests/test_plots/test_map_plot_slab.py
@@ -21,5 +21,13 @@ class Test_MapPlotSlab(unittest.TestCase):
         plt.savefig(join(this_test_path, '~outs/plot_slab_top.png'))
         plt.close()
 
+    def test_plot_dep_contour(self):
+        plot = MapPlotSlab()
+        plot.plot_dep_contour(-20, color='red')
+        plot.plot_dep_contour(-40, color='blue')
+        plot.plot_dep_contour(-60, color='black')
+        plt.savefig(join(this_test_path, '~outs/plot_dep_contour.png'))
+        plt.close()
+
 if __name__ =='__main__':
     unittest.main()
