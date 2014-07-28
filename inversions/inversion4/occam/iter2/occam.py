fb811795436e805092a7a7544743fa3cafdaa5c5
Yang Zhang <zy31415@gmail.com>
2014-07-27 18:43:29 -0700

    .

--- inversions/inversion4/occam/iter2/occam.py
+++ inversions/inversion4/occam/iter2/occam.py
@@ -8,17 +8,17 @@ from viscojapan.inversion.basis_function import BasisMatrix
 from epochs_log import epochs 
 from alphas import alphas
 
-fault_file = '../../fault_model/fault_He50km.h5'
+fault_file = 'fault_model/fault_bott33km.h5'
 
 
 basis = BasisMatrix.create_from_fault_file(fault_file, num_epochs = len(epochs))
 
 inv = OccamDeconvolution(
-    file_G0 = '../../green_function/G_He50km_Vis5.8E18_Rake90.h5',
+    file_G0 = 'green_function/G_He33km_Vis5.8E18_Rake83.h5',
     
-    files_Gs = ['../../green_function/G_He50km_Vis1.0E19_Rake90.h5',
-                '../../green_function/G_He55km_Vis5.8E18_Rake90.h5',
-                '../../green_function/G_He50km_Vis5.8E18_Rake95.h5'
+    files_Gs = ['green_function/G_He33km_Vis1.1E19_Rake83.h5',
+                'green_function/G_He45km_Vis5.8E18_Rake83.h5',
+                'green_function/G_He33km_Vis5.8E18_Rake90.h5'
                 ],
     nlin_par_names = ['log10(visM)','He','rake'],
 
@@ -31,6 +31,8 @@ inv = OccamDeconvolution(
     basis = basis,          
     )
 inv.set_data_except_L()
+
+betas = [betas[10]]
 
 for bno, beta in enumerate(betas):
     for ano, alpha in enumerate(alphas):
