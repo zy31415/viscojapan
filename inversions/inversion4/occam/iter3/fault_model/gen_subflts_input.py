a7b38dcdf7572beda56ce317e2fcddf0e06e8752
Yang Zhang <zy31415@gmail.com>
2014-07-30 02:41:18 -0700

    .

--- inversions/inversion4/occam/iter3/fault_model/gen_subflts_input.py
+++ inversions/inversion4/occam/iter3/fault_model/gen_subflts_input.py
@@ -2,7 +2,7 @@ from viscojapan.pollitz import gen_subflts_input_for_pollitz
 
 gen_subflts_input_for_pollitz(
     fault_file = 'fault_bott40km.h5',
-    out_dir = 'subflts_bott40km_rake80',
+    out_dir = 'subflts_bott40km_rake83',
     rake = 83.)
 
 gen_subflts_input_for_pollitz(
