92ceeb4ef98dc4e2ebf805d8e0b930895ddd3544
Yang Zhang <zy31415@gmail.com>
2014-08-11 20:43:27 -0700

    .

--- lib/viscojapan/inversion/regularization/__init__.py
+++ lib/viscojapan/inversion/regularization/__init__.py
@@ -2,5 +2,5 @@ from .regularization import Composite
 from .roughening import Roughening, ExpandForAllEpochs
 from .temporal_regularization import TemporalRegularization
 from .utils import create_roughening_temporal_regularization,\
-     create_roughening_temporal_intensity_regularization
+     create_temporal_damping_roughening_regularization
 from .intensity import Intensity
--- lib/viscojapan/inversion/regularization/utils.py
+++ lib/viscojapan/inversion/regularization/utils.py
@@ -17,20 +17,38 @@ def create_roughening_temporal_regularization(
     
     return reg
 
-def create_roughening_temporal_intensity_regularization(
-    fault_file, epochs, rough, temp, intensity):
+##def create_roughening_temporal_intensity_regularization(
+##    fault_file, epochs, rough, temp, intensity):
+##
+##    reg_rough = Roughening.create_from_fault_file(fault_file)
+##    exp_reg_rough = ExpandForAllEpochs(reg = reg_rough, num_epochs = len(epochs))
+##
+##    reg_temp = TemporalRegularization.create_from_fault_file(fault_file, epochs)
+##
+##    reg_intensity = Intensity.create_from_fault_file(fault_file)
+##    exp_reg_inten = ExpandForAllEpochs(reg = reg_intensity, num_epochs = len(epochs))
+##    
+##    reg = Composite().\
+##          add_component(exp_reg_rough, arg=rough, arg_name='roughening').\
+##          add_component(reg_temp, arg=temp, arg_name='temporal').\
+##          add_component(exp_reg_inten, arg=intensity, arg_name='intensity')
+##    
+##    return reg
 
-    reg_rough = Roughening.create_from_fault_file(fault_file)
-    exp_reg_rough = ExpandForAllEpochs(reg = reg_rough, num_epochs = len(epochs))
+def create_temporal_damping_roughening_regularization(
+    fault_file, epochs, temp, damp, rough):
 
     reg_temp = TemporalRegularization.create_from_fault_file(fault_file, epochs)
 
     reg_intensity = Intensity.create_from_fault_file(fault_file)
     exp_reg_inten = ExpandForAllEpochs(reg = reg_intensity, num_epochs = len(epochs))
     
+    reg_rough = Roughening.create_from_fault_file(fault_file)
+    exp_reg_rough = ExpandForAllEpochs(reg = reg_rough, num_epochs = len(epochs))
+ 
     reg = Composite().\
-          add_component(exp_reg_rough, arg=rough, arg_name='roughening').\
-          add_component(reg_temp, arg=temp, arg_name='temporal').\
-          add_component(exp_reg_inten, arg=intensity, arg_name='intensity')
-    
+          add_component(reg_temp, arg=temp, arg_name='temporal'). \
+          add_component(exp_reg_inten, arg=damp, arg_name='damping'). \
+          add_component(exp_reg_rough, arg=rough, arg_name='roughening')
+          
     return reg
