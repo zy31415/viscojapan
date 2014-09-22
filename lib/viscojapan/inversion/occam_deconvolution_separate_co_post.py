efe531d099f3731ea100aecd6038ac3b27a5abe4
Yang Zhang <zy31415@gmail.com>
2014-09-22 15:16:50 -0700

    .

--- lib/viscojapan/inversion/occam_deconvolution_separate_co_post.py
+++ lib/viscojapan/inversion/occam_deconvolution_separate_co_post.py
@@ -24,6 +24,7 @@ class OccamDeconvolutionSeparateCoPost(OccamDeconvolution):
                          file_d, file_sd, filter_sites_file, epochs,
                          regularization, basis, file_incr_slip0)
 
+        assert self.epochs[0] == 0, 'The first epoch must be zero.'
         self.num_epochs = len(self.epochs) - 1
 
         ep = vj.EpochalIncrSlip(file_incr_slip0)
@@ -39,6 +40,17 @@ class OccamDeconvolutionSeparateCoPost(OccamDeconvolution):
     def set_data_d(self):
         super().set_data_d()
         self.d = self.d[self.num_obs:,:]
+
+        # subtracting coseismic effect:
+        ep = vj.EpochalG(self.file_G0, self.filter_sites_file)
+        co_slip = vj.EpochalIncrSlip(self.file_incr_slip0)[0]
+        
+        for nth, epoch in enumerate(self.epochs[1:]):
+            G = ep[epoch]
+            d_co = np.dot(G,co_slip)
+            self.d[self.num_obs*nth : self.num_obs*(nth+1),:] -= d_co
+        assert self.num_obs*(nth+1) == self.num_obs*self.num_epochs
+            
         self.disp_obs = self.disp_obs[self.num_obs:,:]
 
     def set_data_sd(self):
