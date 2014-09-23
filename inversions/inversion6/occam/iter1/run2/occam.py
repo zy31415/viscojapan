5419fb62eee059d242ef9a4048086e424647f4e8
Yang Zhang <zy31415@gmail.com>
2014-09-22 21:57:20 -0700

    .

--- inversions/inversion6/occam/iter1/run2/occam.py
+++ inversions/inversion6/occam/iter1/run2/occam.py
@@ -8,22 +8,22 @@ from viscojapan.inversion.basis_function import BasisMatrix
 from epochs import epochs
 from reg_roughes import reg_roughes
 
-fault_file = 'fault_model/fault_bott50km.h5'
+fault_file = '../fault_model/fault_bott50km.h5'
 
-basis = BasisMatrix.create_from_fault_file(fault_file, num_epochs = len(epochs)-1)
+basis = BasisMatrix.create_from_fault_file(fault_file, num_epochs = len(epochs))
 
 inv = vj.OccamDeconvolutionSeparateCoPost(
-    file_G0 = 'green_function/G_He50km_Vis5.8E18_Rake90.h5',
+    file_G0 = '../green_function/G_He50km_Vis5.8E18_Rake90.h5',
     
-    files_Gs = ['green_function/G_He50km_Vis1.0E19_Rake90.h5',
-                'green_function/G_He55km_Vis5.8E18_Rake90.h5',
-                'green_function/G_He50km_Vis5.8E18_Rake95.h5'
+    files_Gs = ['../green_function/G_He50km_Vis1.0E19_Rake90.h5',
+                '../green_function/G_He55km_Vis5.8E18_Rake90.h5',
+                '../green_function/G_He50km_Vis5.8E18_Rake95.h5'
                 ],
     nlin_par_names = ['log10(visM)','He','rake'],
 
-    file_d = '../cumu_post_with_seafloor.h5',
-    file_sd = '../sites_sd/sites_sd.h5',
-    file_incr_slip0 = 'slip0/incr_slip0.h5',
+    file_d = 'obs/post_obs.h5',
+    file_sd = '../../sites_sd/sites_sd.h5',
+    file_incr_slip0 = '../slip0/incr_slip0.h5',
     filter_sites_file = 'sites',
     epochs = epochs,
     regularization = None,
@@ -41,7 +41,7 @@ for nrough, reg_rough in enumerate(reg_roughes):
     print(outfname)
     inv.regularization = \
            reg = vj.create_temporal_edge_roughening(
-               fault_file, epochs[1:], reg_temp, reg_edge, reg_rough)
+               fault_file, epochs, reg_temp, reg_edge, reg_rough)
     
     inv.set_data_L()
     inv.run()
