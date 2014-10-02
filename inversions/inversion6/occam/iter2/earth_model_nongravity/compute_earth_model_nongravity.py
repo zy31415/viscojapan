be2a942561d9322fc873e97a29e19439a477636b
Yang Zhang <zy31415@gmail.com>
2014-10-02 14:57:46 -0700

    .

--- inversions/inversion6/occam/iter2/earth_model_nongravity/compute_earth_model_nongravity.py
+++ inversions/inversion6/occam/iter2/earth_model_nongravity/compute_earth_model_nongravity.py
@@ -18,103 +18,33 @@ lmax = 900
 cmd1={}
 cmd2={}
 
-######################
-mod_str = 'He50km_Vis2.8E19'
 
-cmd1[mod_str]= stat0A(
-    earth_model_stat = join(mod_str, 'earth.model_'+mod_str),
-    stat0_out = join(mod_str, 'stat0.out'),
-    l_min = 1,
-    l_max = 15000,
-    fault_bottom_depth = fault_bottom_depth,
-    fault_top_depth = fault_top_depth,
-    obs_dep = 0.,
-    if_skip_on_existing_output = True,
-    stdout = FNULL
-    )
+def add_task(mod_str, lmax):
+    cmd1[mod_str]= stat0A(
+        earth_model_stat = join(mod_str, 'earth.model_'+mod_str),
+        stat0_out = join(mod_str, 'stat0.out'),
+        l_min = 1,
+        l_max = 15000,
+        fault_bottom_depth = fault_bottom_depth,
+        fault_top_depth = fault_top_depth,
+        obs_dep = 0.,
+        if_skip_on_existing_output = True,
+        stdout = FNULL
+        )
 
-cmd2[mod_str] = vj.pollitz.ComputeEarthModelVISCO1DNonGravity(
-    earth_file = join(mod_str, 'earth.model_'+mod_str),
-    l_max = lmax,
-    outputs_dir = mod_str,
-    if_skip_on_existing_output = True,
-    stdout = FNULL,
-    stderr = FNULL,
-    )
+    cmd2[mod_str] = vj.pollitz.ComputeEarthModelVISCO1DNonGravity(
+        earth_file = join(mod_str, 'earth.model_'+mod_str),
+        l_max = lmax,
+        outputs_dir = mod_str,
+        if_skip_on_existing_output = True,
+        stdout = FNULL,
+        stderr = FNULL,
+        )
 
-######################
-mod_str = 'He50km_Vis4.0E19'
+add_task('He50km_Vis2.8E19', lmax)
+add_task('He50km_Vis4.0E19', lmax)
+add_task('He45km_Vis2.8E19', lmax)
 
-cmd1[mod_str]= stat0A(
-    earth_model_stat = join(mod_str, 'earth.model_'+mod_str),
-    stat0_out = join(mod_str, 'stat0.out'),
-    l_min = 1,
-    l_max = 15000,
-    fault_bottom_depth = fault_bottom_depth,
-    fault_top_depth = fault_top_depth,
-    obs_dep = 0.,
-    if_skip_on_existing_output = True,
-    stdout = FNULL
-    )
-
-cmd2[mod_str] = vj.pollitz.ComputeEarthModelVISCO1DNonGravity(
-    earth_file = join(mod_str, 'earth.model_'+mod_str),
-    l_max = lmax,
-    outputs_dir = mod_str,
-    if_skip_on_existing_output = True,
-    stdout = FNULL,
-    stderr = FNULL,
-    )
-
-######################
-mod_str = 'He45km_Vis2.8E19'
-
-cmd1[mod_str]= stat0A(
-    earth_model_stat = join(mod_str, 'earth.model_'+mod_str),
-    stat0_out = join(mod_str, 'stat0.out'),
-    l_min = 1,
-    l_max = 15000,
-    fault_bottom_depth = fault_bottom_depth,
-    fault_top_depth = fault_top_depth,
-    obs_dep = 0.,
-    if_skip_on_existing_output = True,
-    stdout = FNULL
-    )
-
-cmd2[mod_str] = vj.pollitz.ComputeEarthModelVISCO1DNonGravity(
-    earth_file = join(mod_str, 'earth.model_'+mod_str),
-    l_max = lmax,
-    outputs_dir = mod_str,
-    if_skip_on_existing_output = True,
-    stdout = FNULL,
-    stderr = FNULL,
-    )
-
-######################
-mod_str = 'He55km_Vis2.8E19'
-
-cmd1[mod_str]= stat0A(
-    earth_model_stat = join(mod_str, 'earth.model_'+mod_str),
-    stat0_out = join(mod_str, 'stat0.out'),
-    l_min = 1,
-    l_max = 15000,
-    fault_bottom_depth = fault_bottom_depth,
-    fault_top_depth = fault_top_depth,
-    obs_dep = 0.,
-    if_skip_on_existing_output = True,
-    stdout = FNULL
-    )
-
-cmd2[mod_str] = vj.pollitz.ComputeEarthModelVISCO1DNonGravity(
-    earth_file = join(mod_str, 'earth.model_'+mod_str),
-    l_max = 730,
-    outputs_dir = mod_str,
-    if_skip_on_existing_output = True,
-    stdout = FNULL,
-    stderr = FNULL,
-    )
-
-###################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute earth model.')
     parser.add_argument('model', type=str, nargs=1,
