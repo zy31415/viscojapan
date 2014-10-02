fcb93e89a14544aa63941c6a1f60d4d68c12fcdb
Yang Zhang <zy31415@gmail.com>
2014-10-01 20:30:13 -0700

    .

--- inversions/inversion6/occam/iter2/green_function/compute_greens_func.py
+++ inversions/inversion6/occam/iter2/green_function/compute_greens_func.py
@@ -87,6 +87,24 @@ model3 = ComputeGreensFunction(
     controller_file = 'pool.config',
     )
 
+## Model Three - Variation on rake:
+## (1) viscosity - 2.7542287e+19 (10^19.44) Pa.s
+## (2) elastic depth - 55km
+## (3) rake - 90.
+
+mod_str = 'He55km_Vis2.8E19'
+earth_file_dir = join('../earth_model/', mod_str)
+
+model4 = ComputeGreensFunction(
+    epochs = epochs,
+    file_sites = 'stations.in',
+    earth_file = join(earth_file_dir, 'earth.model_' + mod_str),
+    earth_file_dir = earth_file_dir,
+    outputs_dir = 'outs_'+mod_str+'_Rake90',
+    subflts_files = subflts_files_rake90,
+    controller_file = 'pool.config',
+    )
+
 ###################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compute G matrix.')
