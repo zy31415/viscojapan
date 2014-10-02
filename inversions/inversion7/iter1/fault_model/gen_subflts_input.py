3632f3f5002aa0bb4eb3b31242513598087d6d9c
Yang Zhang <zy31415@gmail.com>
2014-10-01 22:37:29 -0700

    .

---
+++ inversions/inversion7/iter1/fault_model/gen_subflts_input.py
@@ -0,0 +1,11 @@
+from viscojapan.pollitz import gen_subflts_input_for_pollitz
+
+gen_subflts_input_for_pollitz(
+    fault_file = 'fault_bott50km.h5',
+    out_dir = 'subflts_rake90',
+    rake = 90.)
+
+gen_subflts_input_for_pollitz(
+    fault_file = 'fault_bott50km.h5',
+    out_dir = 'subflts_rake95',
+    rake = 95.)
