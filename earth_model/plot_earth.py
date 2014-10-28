24966e9ed34da9a014618c0d496e51a1fb94aea3
Yang Zhang <zy31415@gmail.com>
2014-10-28 16:15:16 -0700

    .

--- earth_model/plot_earth.py
+++ earth_model/plot_earth.py
@@ -16,30 +16,37 @@ rL=rE-60.
 x=rL*cos(theta)
 y=rL*sin(theta)
 plot(x,y)
+annotate('Elastic Lithosphere', (-555,6283.17), (-1200, 6500),
+         arrowprops=dict(arrowstyle="->"),
+         fontsize = 12)
 
 # asthenosphere
 rA=rE-220.
 x=rA*cos(theta)
 y=rA*sin(theta)
 plot(x,y)
+text(-220, 6200, 'Asthenosphere', fontsize=9)
 
 # upper mantle
 rA=rE-670.
 x=rA*cos(theta)
 y=rA*sin(theta)
 plot(x,y)
+text(-350, 5850,'Upper Mantle')
 
 # core-mantle
 rA=rE-2900.
 x=rA*cos(theta)
 y=rA*sin(theta)
 plot(x,y)
+text(100, 4500,'Mantle')
+text(100, 3200,'Core')
 
 # mark epicentor:
 theta=pi/2
 x=rE*cos(theta)
 y=rE*sin(theta)
-plot(x,y,marker='s',ms=8,label='Epi',ls='')
+plot(x,y,marker='s',ms=8,label='Epicenter',ls='')
 
 # mark J550:
 from pyproj import Geod
@@ -75,9 +82,12 @@ dis=tp[2]
 theta=pi/2+dis/1e3/rE
 x=rE*cos(theta)
 y=rE*sin(theta)
-plot(x,y,marker='^',ms=8,label='ULAB',ls='')
+plot(x,y,marker='^',ms=8,label='ULAB, 3034km',ls='')
 
 axis('equal')
 axvline(0.,ls='--',color='gray')
-legend(loc=3,numpoints=1)
+legend(loc=3,numpoints=1, prop={'size':10})
+ylim([3000, 7000])
+
+savefig('spherical_effec.pdf')
 show()
