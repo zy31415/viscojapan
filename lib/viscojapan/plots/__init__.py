2477424e16b9a6e76867bf26a4023251fbcc8b04
Yang Zhang <zy31415@gmail.com>
2014-10-29 14:33:12 -0700

    .

--- lib/viscojapan/plots/__init__.py
+++ lib/viscojapan/plots/__init__.py
@@ -1,13 +1,13 @@
 try:
     from pylab import plt
+    from .my_basemap import *
+    from .map_plot import MapPlot
+    from .map_plot_slab import MapPlotSlab
+    from .map_plot_fault import MapPlotFault
+    from .map_plot_sites import *
+    from .plot_L_curve import plot_L
+    from .plot_coseismic_disp import *
+    from .utils import *
+    from .others import *
 except:
-    pass
-from .my_basemap import *
-from .map_plot import MapPlot
-from .map_plot_slab import MapPlotSlab
-from .map_plot_fault import MapPlotFault
-from .map_plot_sites import *
-from .plot_L_curve import plot_L
-from .plot_coseismic_disp import *
-from .utils import *
-from .others import *
+    print('No pylab. Cannot plot.')
