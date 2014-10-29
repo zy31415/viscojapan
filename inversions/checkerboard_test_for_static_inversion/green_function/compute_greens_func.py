223fd33a03eb93cf2bd2a11cfec7b9b5bf94947c
Yang Zhang <zy31415@gmail.com>
2014-10-29 14:50:52 -0700

    acp commit

--- inversions/checkerboard_test_for_static_inversion/green_function/compute_greens_func.py
+++ inversions/checkerboard_test_for_static_inversion/green_function/compute_greens_func.py
@@ -20,7 +20,7 @@ def add_task(mod_str, rake, model_num):
     subflts_files = globals()['subflts_files_rake%02d'%rake]
     model = ComputeGreensFunction(
         epochs = epochs,
-        file_sites = 'stations.in',
+        file_sites = 'sites',
         earth_file = join(earth_file_dir, 'earth.model_' + mod_str),
         earth_file_dir = earth_file_dir,
         outputs_dir = 'outs%d_'%model_num + mod_str+'_Rake%2d'%rake,
