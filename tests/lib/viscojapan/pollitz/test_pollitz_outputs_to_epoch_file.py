a656f2732c1d9f6eb588a4d887431da024b40704
Yang Zhang <zy31415@gmail.com>
2014-10-29 19:26:02 -0700

    acp commit

---
+++ tests/lib/viscojapan/pollitz/test_pollitz_outputs_to_epoch_file.py
@@ -0,0 +1,42 @@
+import unittest
+from os.path import join
+
+import numpy as np
+
+from viscojapan.pollitz import PollitzOutputsToEpochalData
+from viscojapan.test_utils import MyTestCase
+
+class Test_PollitzOutputsToEpochalData(MyTestCase):
+    def setUp(self):
+        self.this_script = __file__
+        super().setUp()
+        self.clean_outs_dir()
+
+    def test(self):
+        visM = 1E19
+        visK = 5E17
+        rake = 90
+        model = PollitzOutputsToEpochalData(                
+            epochs = [0, 60],
+            G_file = join(self.outs_dir, 'G.h5'),
+            num_subflts = 10,
+            pollitz_outputs_dir = join(self.share_dir, 'pollitz_outs'),
+            sites_file = join(self.share_dir,'sites'),
+            extra_info ={
+            'He':50,
+            'visM':visM,
+            'log10(visM)':np.log10(visM),
+            'visK':visK,
+            'log10(visK)':np.log10(visK),
+            'rake':rake
+            },
+            extra_info_attrs ={
+            'He':{'unit':'km'},
+            'visM':{'unit':'Pa.s'},
+            'visK':{'unit':'Pa.s'},
+            }
+            )
+        model()
+            
+if __name__ == '__main__':
+    unittest.main()
