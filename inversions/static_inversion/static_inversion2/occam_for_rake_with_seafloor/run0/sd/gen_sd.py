e749c7869a0fe404e924662684003938ebd409ce
Yang Zhang <zy31415@gmail.com>
2014-10-31 14:45:44 -0700

    acp commit

--- inversions/static_inversion/static_inversion2/occam_for_rake_with_seafloor/run0/sd/gen_sd.py
+++ inversions/static_inversion/static_inversion2/occam_for_rake_with_seafloor/run0/sd/gen_sd.py
@@ -1,3 +1,5 @@
+from os.path import exists
+
 import viscojapan as vj
 
 sites = vj.Sites.init_from_txt('stations.in')
@@ -11,4 +13,15 @@ gen = vj.tsana.GenUniformOnshoreSDWithInfiniteSeafloorSD(
     sd_post_ver = 5
     )
 
-gen.save('sd_seafloor_inf.h5')
+file_sd_seafloor_inf = 'sd_seafloor_inf.h5'
+if not exists:
+    gen.save(file_sd_seafloor_inf)
+
+
+vj.tsana.copy_and_revise_sd_file(
+    'sd_seafloor_inf.h5',
+    'sites_seafloor',
+    'sd_seafloor_01.h5',
+    (1.,1.,5.)
+    )
+    
