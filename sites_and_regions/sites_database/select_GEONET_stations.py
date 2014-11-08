35ad88ac8fee862ac3382de72ec1fe90ef03da24
Yang Zhang <zy31415@gmail.com>
2014-11-07 17:28:33 -0800

    acp commit

--- sites_and_regions/sites_database/select_GEONET_stations.py
+++ sites_and_regions/sites_database/select_GEONET_stations.py
@@ -18,10 +18,14 @@ def check_in_GEONET(lon, lat):
     nankaido = (138.52, 41.25, 147.89, 45.78)
     honshu = (132.57, 32.19, 142.76, 41.55)
     kyushu = (128.34, 29.35, 132.54, 34.77)
-    
+    ryukyu_trench = (122.20, 23.31, 130.92, 28.18)
     if check_if_in_region(lon, lat, *nankaido):
         return True
     if check_if_in_region(lon, lat, *honshu):
+        return True
+    if check_if_in_region(lon, lat, *kyushu):
+        return True
+    if check_if_in_region(lon, lat, *ryukyu_trench):
         return True
     return False
 
