fa22bf9b67acf00825f2723e9d729dc0e57faf42
Yang Zhang <zy31415@gmail.com>
2014-11-07 16:42:34 -0800

    acp commit

--- sites_and_regions/sites_database/backup.sh
+++ sites_and_regions/sites_database/backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+rm -f database_backup.schema
 sqlite3 gps_sites.db <<!
 .headers on
 .output database_backup.schema
 .schema
 !
 
+rm -f database_backup.data
 sqlite3 gps_sites.db <<!
 .headers on
 .output database_backup.data
