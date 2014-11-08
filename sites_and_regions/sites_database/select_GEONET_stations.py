7a3acb868fdd72bcc7e2e15167b4ff205d822126
Yang Zhang <zy31415@gmail.com>
2014-11-07 17:25:04 -0800

    .

---
+++ sites_and_regions/sites_database/select_GEONET_stations.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+__doc__ = 'Select GEONET stations and add into the database'
+
+def get_sites_pos_dic():
+    with sqlite3.connect('gps_sites.db') as conn:
+        c = conn.cursor()
+        c.execute('PRAGMA foreign_keys = ON;')
+        sites_pos = c.execute('select * from tb_sites;').fetchall()
+    return {ii[0]:(ii[1],ii[2]) for ii in sites_pos}
+    
+def check_if_in_region(lon, lat, west, south, east, north):
+    if (lon>west) and (lon<east) and (lat>south) and (lat<north):
+        return True
+    return False
+
+def check_in_GEONET(lon, lat):
+    nankaido = (138.52, 41.25, 147.89, 45.78)
+    honshu = (132.57, 32.19, 142.76, 41.55)
+    kyushu = (128.34, 29.35, 132.54, 34.77)
+    
+    if check_if_in_region(lon, lat, *nankaido):
+        return True
+    if check_if_in_region(lon, lat, *honshu):
+        return True
+    return False
+
+sites_GEONET = []
+    
+sites_pos = get_sites_pos_dic()
+for site, pos in sites_pos.items():
+    if check_in_GEONET(*pos):
+        sites_GEONET.append(site)
+
+with sqlite3.connect('gps_sites.db') as conn:
+    c = conn.cursor()
+    c.execute('PRAGMA foreign_keys = ON;')
+    for site in sites_GEONET:
+        try:
+            c.execute('insert into tb_networks values (?, "GEONET", "");',
+                      (site,))
+        except sqlite3.IntegrityError:
+            print('%s is already in GEONET.'%site)
+            pass
+    conn.commit()
+
