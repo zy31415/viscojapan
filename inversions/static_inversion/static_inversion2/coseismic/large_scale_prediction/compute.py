878d78cbf221d15821757248b495687d872a7376
Yang Zhang <zy31415@gmail.com>
2014-11-17 19:54:54 -0800

    .

--- inversions/static_inversion/static_inversion2/coseismic/large_scale_prediction/compute.py
+++ inversions/static_inversion/static_inversion2/coseismic/large_scale_prediction/compute.py
@@ -1,10 +1,12 @@
 from os.path import join
+import glob
 
 import viscojapan as vj
 
 earth_file_dir = '../../earth_model_nongravity/He63km_VisM1.0E19/'
 earth_file = join(earth_file_dir, 'earth.model_He63km_VisM1.0E19')
 
+subflts_files = glob.glob('subflts/flt_????')
 
 cmd = vj.pollitz.ComputeGreensFunction(
     epochs = [0],
@@ -12,7 +14,7 @@ cmd = vj.pollitz.ComputeGreensFunction(
     earth_file = earth_file,
     earth_file_dir = earth_file_dir,
     outputs_dir = 'outs',
-    subflts_files = 'subflts',
+    subflts_files = subflts_files,
     controller_file = 'pool.config',)
 
 if __name__ == '__main__':
