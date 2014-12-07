436c0aaef6c2deb4b7ff30f358b0557a9ef3a748
Yang Zhang <zy31415@gmail.com>
2014-12-06 21:51:10 -0800

    acp commit

--- lib/viscojapan/inversion/__init__.py
+++ lib/viscojapan/inversion/__init__.py
@@ -4,7 +4,10 @@ from .occam_deconvolution_separate_co_post2 import *
 from .static_inversion import StaticInversion
 from .occam_deconvolution import *
 from .result_file import *
-from .predict_displacement import *
+try:
+    from .predict_displacement import *
+except ImportError:
+    print("    Cannot import sqlite3.")
 
 from . import regularization as reg
 from . import basis_function as basis
