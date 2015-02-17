2cf739345085be3e67434d2be1afe036de72ee18
Yang Zhang <zy31415@gmail.com>
2015-02-16 19:47:36 -0800

    Add GG in class CvxoptQpWrapper

--- lib/viscojapan/inversion/cvxopt_qp_wrapper.py
+++ lib/viscojapan/inversion/cvxopt_qp_wrapper.py
@@ -7,10 +7,12 @@ from ..utils import assert_col_vec_and_get_nrow,\
 class CvxoptQpWrapper(object):
     def __init__(self,*,
                  P,
-                 q
+                 q,
+                 GG = None
                  ):
         self.P = P
         self.q = q
+        self.GG = GG
         
         self._check_input()
 
@@ -20,15 +22,29 @@ class CvxoptQpWrapper(object):
         assert  nrow_P == nrow_q
         self.nrow_P = nrow_P
 
+        if self.GG is not None:
+            assert self.GG.shape[1] == nrow_P
+
+
     def invert(self, nonnegative=True):
         # non-negative constraint
         if nonnegative:
             GG = -1.0 * np.identity(self.nrow_P, dtype='float')
+            if self.GG is not None:
+                GG = np.vstack([GG, self.GG])
+
             h = np.zeros((self.nrow_P, 1), dtype='float')
             self.solution = solvers.qp(matrix(self.P),matrix(self.q),
                                        matrix(GG),matrix(h))
         else:
-            self.solution = solvers.qp(matrix(self.P),matrix(self.q))
+            if self.GG is not None:
+                self.GG = matrix(self.GG)
+
+            self.solution = solvers.qp(
+                matrix(self.P),
+                matrix(self.q),
+                G = self.GG
+            )
 
     @classmethod
     def create_from_inversion_parameters_set(cls, inv_par_set):
