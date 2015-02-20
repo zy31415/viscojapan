de624f7396c89b87191db1d7db1b98d16e10f961
Yang Zhang <zy31415@gmail.com>
2015-02-19 16:33:20 -0800

    acp commit

--- inversions/inversion10/iter2/run9/analysis/pred_disp_large_scale/deformation_partitioner.py
+++ inversions/inversion10/iter2/run9/analysis/pred_disp_large_scale/deformation_partitioner.py
@@ -6,13 +6,14 @@ import viscojapan as vj
 
 slip_file = '../slip_extrapolation/extra_slip_EXP.h5'
 slip = vj.epoch_3d_array.Slip.load(slip_file)
+epochs = slip.get_epochs()
 
 result_file = '../../outs/nrough_06_naslip_11.h5'
 result_file_reader = vj.inv.ResultFileReader(result_file)
 
 pred = vj.inv.DeformPartitioner(
-    epochs = slip.get_epochs()
-    slip = slip
+    epochs = epochs,
+    slip = slip,
     nlin_pars = result_file_reader.nlin_par_solved_values,
     nlin_par_names = result_file_reader.nlin_par_names,
     file_G0 = '../../../green_function_large_scale/G0_He50km_VisM6.3E18_Rake83.h5',
