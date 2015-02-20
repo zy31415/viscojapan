0f4d66bba0729d6c3265cd8ec8db32cececc2417
Yang Zhang <zy31415@gmail.com>
2015-02-19 16:32:24 -0800

    .

--- inversions/inversion10/iter2/run9/analysis/pred_disp_large_scale/deformation_partitioner.py
+++ inversions/inversion10/iter2/run9/analysis/pred_disp_large_scale/deformation_partitioner.py
@@ -4,10 +4,17 @@ import numpy as np
 
 import viscojapan as vj
 
-result_file = './slip_extrapolation/extra_slip_EXP.h5'
+slip_file = '../slip_extrapolation/extra_slip_EXP.h5'
+slip = vj.epoch_3d_array.Slip.load(slip_file)
 
-pred = vj.inv.DeformPartitionerForResultFile(
-    result_file = result_file,
+result_file = '../../outs/nrough_06_naslip_11.h5'
+result_file_reader = vj.inv.ResultFileReader(result_file)
+
+pred = vj.inv.DeformPartitioner(
+    epochs = slip.get_epochs()
+    slip = slip
+    nlin_pars = result_file_reader.nlin_par_solved_values,
+    nlin_par_names = result_file_reader.nlin_par_names,
     file_G0 = '../../../green_function_large_scale/G0_He50km_VisM6.3E18_Rake83.h5',
     files_Gs = ['../../../green_function_large_scale/G1_He50km_VisM1.0E19_Rake83.h5',
                 '../../../green_function_large_scale/G2_He60km_VisM6.3E18_Rake83.h5',
