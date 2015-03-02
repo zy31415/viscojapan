8f417f51dc1cd14703aa137c580e678b43194614
Yang Zhang <zy31415@gmail.com>
2015-03-01 21:49:33 -0800

    .

--- inversions/inversion10/iter3/fault_model/gen_subflts_input.py
+++ inversions/inversion10/iter3/fault_model/gen_subflts_input.py
@@ -2,12 +2,15 @@ from viscojapan.pollitz import gen_subflts_input
 
 dep = 80
 
-gen_subflts_input(
-    fault_file = 'fault_bott%dkm.h5'%dep,
-    out_dir = 'subflts_bott%dkm_rake83'%dep,
-    rake = 83)
+rake1 = 84
+rake2 = 85
 
-gen_subflts_input(
-    fault_file = 'fault_bott%dkm.h5'%dep,
-    out_dir = 'subflts_bott%dkm_rake90'%dep,
-    rake = 90.)
+
+def gen(rake):
+    gen_subflts_input(
+        fault_file = 'fault_bott%dkm.h5'%dep,
+        out_dir = 'subflts_bott%dkm_rake%d'%(dep, rake),
+        rake = rake)
+
+gen(rake1)
+gen(rake2)
