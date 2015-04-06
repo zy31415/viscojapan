affa6bff9ab5e2e90cfd0297b021edb4d337b315
Yang Zhang <zy31415@gmail.com>
2015-04-05 19:46:33 -0700

    acp commit

--- inversions/inversion10/iter2_corr/earth_model_nongravity/compute_earth_model.py
+++ inversions/inversion10/iter2_corr/earth_model_nongravity/compute_earth_model.py
@@ -7,7 +7,7 @@ import viscojapan as vj
 
 FNULL = open(os.devnull, 'w')
 
-fid = vm.fm.FaultFileReader('../fault_model/fault_bott80km.h5')
+fid = vj.fm.FaultFileReader('../fault_model/fault_bott80km.h5')
 fault_bottom_depth = fid.depth_bottom
 fault_top_depth = fid.depth_top
 
